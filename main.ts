0f08757280f896df46204499642a55536795b3f0
VicRoger27 <121492850+VicRoger27@users.noreply.github.com>
2023-01-30 12:07:00 +0000

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,3 @@
+basic.forever(function () {
+    music.playSoundEffect(music.createSoundEffect(WaveShape.Sawtooth, 1623, 975, 255, 255, 1427, SoundExpressionEffect.Warble, InterpolationCurve.Linear), SoundExpressionPlayMode.UntilDone)
+})
